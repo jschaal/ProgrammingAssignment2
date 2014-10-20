e7425591b12294fe995f9b2ec5bc057e662fbc95
Joe Schaal <joe@schaal.net>
2014-10-20 11:37:52 -0400

    Changed function case to comply with unit test scropt

--- cachematrix.R
+++ cachematrix.R
@@ -11,24 +11,24 @@ makeCacheMatrix <- function(x = matrix()) {
         }
         get <- function() x
         set <- function(newMatrix) m <<- newMatrix
-        getInverse <- function() m
-        setInverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        setinverse <- function(inverse) m <<- inverse
         list(set = set, get = get,
-             getInverse = getInverse,
-             setInverse = setInverse)
+             getinverse = getinverse,
+             setinverse = setinverse)
 }
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        m <- x$getInverse()
+        m <- x$getinverse()
         if(!is.null(m)) {
                 message("getting cached data")
                 return(m)
         }
         data <- x$get()
         m <- solve(data)
-        x$setInverse(m)
+        x$setinverse(m)
         m
 }
